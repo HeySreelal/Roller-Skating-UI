71378b0f0aa2908bd4c12e56762b8af1995067df
Sreelal TS <heysreelal@gmail.com>
2022-04-14 23:40:25 +0530

    Minor fix 🎨

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Roller Skating',
       theme: SkatingTheme.theme,
       home: const GetStarted(),
